0e59ced5f6f6dbc90fda59d501881a1f17cf3be5
Manjunath K <manjunathmayya@gmail.com>
2021-05-10 10:27:48 +0530

    Creating initial repository for Cyber dojo session of sweet and spicy

---
+++ NUnit/Sweet_and_Spicy/WordWrap_round2.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+/*
+ * Your task is to write a function that takes two arguments, a string and an integer width.
+The function returns the string, 
+but with line breaks inserted at just the right places 
+to make sure that no line is longer than the column number. 
+You try to break lines at word boundaries.
+Like a word processor, break the line by replacing the last space in a line with a newline.
+ */
+namespace Sweet_and_Spicy
+{
+    [TestFixture]
+    public class WordWrap_round2
+    {
+        [Test]
+        public void Given_When_Then()
+        {
+            Assert.AreEqual("42",Answer());
+        }
+
+        private string Answer()
+        {
+            return "?";
+        }
+    }
+}
