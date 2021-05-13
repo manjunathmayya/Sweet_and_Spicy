fc1676d139f529aa917c731c69a10fea0dd89489
Rashmi HN <rashmihngowda@gmail.com>
2021-05-13 15:28:08 +0530

    Test Staging

--- NUnit/Sweet_and_Spicy/RecentlyUsedList_team2.cs
+++ NUnit/Sweet_and_Spicy/RecentlyUsedList_team2.cs
@@ -20,7 +20,7 @@ namespace Sweet_and_Spicy
         [Test]
         public void Given_When_Then()
         {
-            Assert.AreEqual("42",Answer());
+            Assert.AreEqual("421",Answer());
         }
 
         private string Answer()
